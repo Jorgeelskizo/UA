84852bdddfad81a0c55437bd1fd83da2567c8950
Javier Herrero Ponce <jhp19@gcloud.ua.es>
2024-05-30 18:46:53 +0200

    footer quitado

--- proyecto.php
+++ proyecto.php
@@ -25,7 +25,7 @@ $stmt->close();
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Universidad de Alicante - Proyecto Fin de Grado Ingenieria Multimedia</title>
+<title>Detalles de proyecto</title>
 <link rel="stylesheet" href="estilos/unificado.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
@@ -238,8 +238,6 @@ $stmt->close();
       </div>
     </div>
 </div>
-
-<?php include 'imports/footer.php'; ?>
 
 <script src="script.js"></script>
 
